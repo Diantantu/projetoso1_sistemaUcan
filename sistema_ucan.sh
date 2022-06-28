e3705275a66b4826b199b92f8ef564667418a82d
Duely Diantantu <duelydiantantu18@gmail.com>
2022-06-29 00:06:36 +0100

    Criando script do projeto;\nCriando cabecalho do projeto;\nEscrevendo informacoes do fs ao arquivo info_ucan.txt;

---
+++ sistema_ucan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Autor: Duely Antonio Diantantu
+#ID:28980
+#Turma:B
+#Disciplina:Sistemas Operativos 1
+#Projeto "Sistema Ucan" e uma sistema de auxilio administrativo de uma
+#universidade onde o controle dos recursos de cada sector da universidade e
+#gerenciado por este script tornando possivel separar todos os setores
+#e apenas permitir o acesso a usuarios responsaveis aos respectivos setores
+df -hT > /mnt/sistema_ucan/info_ucan.txt
+echo "--------------------------------------------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------------------------------------------------------------------------------------------------"
